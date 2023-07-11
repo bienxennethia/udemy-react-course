c7d302ff2914e064bea2fd164d6b033ed356932f
Xennethia Bien <bienxennethia@gmail.com>
2023-07-11 16:59:29 +0800

    :art: created expenses chart

--- src/components/Expenses/components/Expenses/Expenses.js
+++ src/components/Expenses/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import './Expenses.css';
 import Card from '../../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -17,6 +18,7 @@ const Expenses = (props) => {
   return (
     <Card className='expenses'>
       <ExpensesFilter selectedYear={filteredYear} onFilterChange={onFilterChangeHandler} />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
