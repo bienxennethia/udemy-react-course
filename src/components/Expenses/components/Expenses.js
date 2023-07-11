18601866ad0ea25e85c1812cec1ddc754beed98b
Xennethia Bien <bienxennethia@gmail.com>
2023-07-11 13:31:16 +0800

    :fire: Assignment working with Events & State

--- src/components/Expenses/components/Expenses.js
+++ src/components/Expenses/components/Expenses.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Expenses.css';
 
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 
+import ExpensesFilter from './ExpensesFilter';
+
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const onFilterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className='expenses'>
+      <ExpensesFilter selectedYear={filteredYear} onFilterChange={onFilterChangeHandler} />
       { 
         props.items.map((expense, key) => <ExpenseItem key={key} title={expense.title} amount={expense.amount} date={expense.date} />)
       }
