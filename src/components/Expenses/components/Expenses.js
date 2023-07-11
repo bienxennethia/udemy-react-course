96b36cc293d8148c492ff140fb034cddeedc1543
Xennethia Bien <bienxennethia@gmail.com>
2023-07-11 15:25:37 +0800

    :fire: Conditional Content

--- src/components/Expenses/components/Expenses.js
+++ src/components/Expenses/components/Expenses.js
@@ -15,12 +15,16 @@ const Expenses = (props) => {
 
   const filteredExpenses = props.items.filter((expense) => expense.date.getFullYear().toString() === filteredYear);
 
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />);
+  }
+
   return (
     <Card className='expenses'>
       <ExpensesFilter selectedYear={filteredYear} onFilterChange={onFilterChangeHandler} />
-      { 
-        filteredExpenses.map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)
-      }
+      { expensesContent }
     </Card>
   );
 }
