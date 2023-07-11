80b40b898c905cd86c263eb9481abc8501466cc0
Xennethia Bien <bienxennethia@gmail.com>
2023-07-11 15:58:18 +0800

    :art: created Expenses folder

--- src/components/Expenses/components/Expenses/Expenses.js
+++ src/components/Expenses/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './Expenses.css';
 
-import Card from '../UI/Card';
+import Card from '../../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
 
